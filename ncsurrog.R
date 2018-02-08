7778eb41f08ebe9a3acea364f89d55d3e6622808
Dan Reuman <reuman@ku.edu>
2018-02-08 13:41:35 -0800

    file management and new function

---
+++ ncsurrog.R
@@ -0,0 +1,101 @@
+require(copula)
+require(mvtnorm)
+
+#This function takes a bunch of time series measured at the same
+#time in different locations and creates surrogate datasets which
+#are statistically similar except the copula has been randomized
+#to a normal copula.
+#
+#Args
+#m          A T by n matrix, where T is the length of the time 
+#             series and n is the number of time series
+#numsurrog  The desired number of surrogate datasets
+#
+#Output
+#A T by n by numsurrog array, surrogs. The ith surrogate is stored
+#surrog[,,i].
+#
+ncsurrog<-function(m,numsurrog)
+{
+  T<-dim(m)[1]
+  n<-dim(m)[2]
+  
+  #get the kendall correlation matrix of the data
+  kcor<-cor(m,use="pairwise.complete.obs",method="kendall")
+  
+  #get the covariance matrix of an mv normal with all
+  #marginal variances 1 and with kendall correlation matrix 
+  #kcor
+  ncov<-iTau(normalCopula(0,2),kcor)
+
+  #generate a bunch of mv normals in the shape of the final
+  #desired output. Each row is a draw from an mv normal with 
+  #mean 0 and cov matrix ncov.
+  surrogs<-array(rmvnorm(T*numsurrog,mean=rep(0,n),sigma=ncov),
+                 dim=c(T,numsurrog,n))
+  surrogs<-aperm(surrogs,c(1,3,2)) #now it is T by n by numsurrog
+  
+  #The nth-smallest element of each time series surrog[,a,b] 
+  #is replaced by the nth-smallest element of m[,a], for all n.
+  #NAs are omitted.
+  for (ca in 1:n)
+  {
+    goodlocs<-is.finite(m[,ca])
+    mgl<-m[goodlocs,ca]
+    omgl<-order(mgl)
+    for (cb in 1:numsurrog)
+    {
+      sgl<-surrogs[goodlocs,ca,cb]
+      osgl<-order(sgl)
+      sgl[osgl]<-mgl[omgl]
+      surrogs[goodlocs,ca,cb]<-sgl
+      surrogs[!goodlocs,ca,cb]<-NA
+    }
+  }
+
+  #***The below is what the code looked like before 
+  #I added the ability handle NAs. I am keeping it around
+  #just in case
+  #
+  #The nth-smallest element of each time series surrog[,a,b] 
+  #is replaced by the nth-smallest element of m[,a], for all n
+  #for (ca in 1:n)
+  #{
+  #  om<-order(m[,ca])
+  #  for (cb in 1:numsurrog)
+  #  {
+  #    os<-order(surrogs[,ca,cb])
+  #    surrogs[os,ca,cb]<-m[om,ca]  
+  #  }
+  #}
+  
+  return(surrogs)
+}
+
+#test without NAs
+cop<-claytonCopula(5,2)
+numpts<-1000
+m<-rCopula(numpts,cop)
+plot(m[,1],m[,2],type='p')
+res<-ncsurrog(m,3)
+dim(res)
+plot(res[,1,1],res[,2,1],type='p')
+ncop<-normalCopula(.5,2)
+BiCopGofTest(res[,1,1],res[,2,1],family=1)
+BiCopGofTest(res[,1,2],res[,2,2],family=1)
+BiCopGofTest(res[,1,3],res[,2,3],family=1)
+
+#test with NAs
+m[sample(1:numpts,5),1]<-NA
+m[sample(1:numpts,5),2]<-NA
+res<-ncsurrog(m,3)
+dim(res)
+sum(is.na(res[,1,1]))
+sum(is.na(res[,2,1]))
+sum(is.na(res[,1,2]))
+sum(is.na(res[,2,2]))
+plot(res[,1,1],res[,2,1],type='p')
+BiCopGofTest(res[,1,1],res[,2,1],family=1)
+BiCopGofTest(res[,1,2],res[,2,2],family=1)
+BiCopGofTest(res[,1,3],res[,2,3],family=1)
+

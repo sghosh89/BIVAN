d8979609429221f32a20d639364765fba003d53d
Dan Reuman <reuman@ku.edu>
2018-03-01 14:22:53 -0800

    added the partial spearman correlation

--- CopulaFunctions_flexible.R
+++ CopulaFunctions_flexible.R
@@ -11,7 +11,7 @@
 #
 #Examples
 #The earlier function D2lD2u should give the combined
-#results of D2(vi,vj,0,.5) and D2(vi,vj,0.5,1).
+#results of D2bds(vi,vj,0,.5) and D2bds(vi,vj,0.5,1).
 #
 D2bds<-function(vi,vj,lb,ub)
 {
@@ -29,18 +29,36 @@ D2bds<-function(vi,vj,lb,ub)
   return(D2)
 }
 
-#Dan to write an analogous Cor function
-
+#Calculates the portion of the Spearman correlation 
+#that is due to points in from a copula that are
+#between two bounds.
+#
 #Args
 #vi, vj       Coordinates of points from a copula
 #lb, ub       Lower and upper bounds between 0 and 1
 #
 #Output
+#The portion of the Spearman correlation.
 #
-
+#Examples
+#The earlier function CorlCoru should give the combined
+#results of Corbds(vi,vj,0,.5) and Corbds(vi,vj,0.5,1).
+#
 Corbds<-function(vi,vj,lb,lu)
 {
+  #get mean and variance
+  vi_mean<-mean(vi)
+  vj_mean<-mean(vj)
+  var_vi<-var(vi)
+  var_vj<-var(vj)
   
+  #compute the indices of the points between the bounds
+  inds<-which(vi+vj>2*lb & vi+vj<2*ub)
+  
+  #get the portion of the Spearman
+  res<-sum((vi[inds]-vi_mean)*(vj[inds]-vj_mean))/((length(vi)-1)*sqrt(var_vi*var_vj))
+
+  return(res)  
 }
 
 #Shya please write an analogous P function, fill in below.

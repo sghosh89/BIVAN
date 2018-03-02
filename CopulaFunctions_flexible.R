4f6c5b2d2fe9beda8141eaf6852376b91d786485
sghosh89 <ghoshshyamolina89@gmail.com>
2018-03-02 16:41:18 -0600

    deleted extra line

--- CopulaFunctions_flexible.R
+++ CopulaFunctions_flexible.R
@@ -229,9 +229,6 @@ Pbds<-function(vi,vj,lb,ub){
   #-----------------------------------------------------------------------
   res<-Au_S-Au_Si
   
-  stepfn_S<-list(dist_S,S)
-  stepfn_Si<-list(dist_Si,Si)
-  
   return(list(dist_S=dist_S,
               S=S,
               dist_Si=dist_Si,

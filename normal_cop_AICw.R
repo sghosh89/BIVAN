d959aa49ad863d307c5a81b12cd531f327f0a6b7
sghosh89 <ghoshshyamolina89@gmail.com>
2018-03-10 19:13:25 -0600

    editted

--- normal_cop_AICw.R
+++ normal_cop_AICw.R
@@ -3,13 +3,14 @@
 #       r is the RDS file which came as an output from FittingCopula_selective_loc.R
 
 normal_cop_AICw_matrix<-function(r){
-  normal_cop_AICw<-matrix(NA, nrow=dim(r$gfc_p_CvM)[1], ncol=dim(r$gfc_p_CvM)[2])
-  colnames(normal_cop_AICw)<-colnames(r$gfc_p_CvM)
-  rownames(normal_cop_AICw)<-rownames(r$gfc_p_CvM)
+  normal_cop_AICw<-r$gfc_numBS
+  #matrix(NA, nrow=dim(r$gfc_p_CvM)[1], ncol=dim(r$gfc_p_CvM)[2])
+  #colnames(normal_cop_AICw)<-colnames(r$gfc_p_CvM)
+  #rownames(normal_cop_AICw)<-rownames(r$gfc_p_CvM)
   
   for(i in c(1:dim(normal_cop_AICw)[1])){
     for(j in c(1:dim(normal_cop_AICw)[1])){
-      if(i!=j){
+      if(is.finite(r$gfc_numBS[i,j])){
         x<-r$info_ord_copcode[i,j,]
         norm_ind<-which(x==1)
         norm_AICw<-r$info_ord_AICw[i,j,norm_ind]
@@ -24,7 +25,7 @@ normal_cop_AICw_matrix<-function(r){
 
 #--------------
 #check the function
-#r<-readRDS("AphidCopulaFit_selecloc_count_species_10.RDS")
+#r<-readRDS("Plankton_North_Sea_CopulaFit_selecloc_species_16.RDS")
 #z<-normal_cop_AICw_matrix(r=r)
 
 

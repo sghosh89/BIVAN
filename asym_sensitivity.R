12b82f00694db6cdf17d8ac8aea61f4ee8697439
sghosh89 <ghoshshyamolina89@gmail.com>
2018-05-08 23:02:35 -0500

    calling noise mat outside the function and then passing as input

--- asym_sensitivity.R
+++ asym_sensitivity.R
@@ -2,9 +2,6 @@
 library(VineCopula)
 library(mvtnorm)
 #----------------------------
-source("OurBiCopSelect.R")
-source("bivfunctionplot.R")
-#----------------------------
 # This function takes a vector (x) and a logical tag and then transform them accordingly
 glob_noise_fn<-function(x,decrease){
   
@@ -45,10 +42,31 @@ glob_noise_fn<-function(x,decrease){
 #g_noise_dec<-apply(mg,MARGIN = 2,FUN = glob_noise_fn, decrease=T)
 #h_noise<-alpha*ml
 #-----------------------------------------------------------------------
+# This function generates global and local noise matrices 
+#            which will be called as input in next simulator function
+# Input:      
+#       N : number of global and local noise points you want
+#       rho : covariance of global noise matrix
+# Output:
+#       a list of two matrices (mg for global noise mat) and (ml for local noise matrix)
+get_noise_glob_loc_mat<-function(N,rho){
+  
+  sigma_glob<-matrix(c(1,rho,rho,1),2,2)
+  sigma_loc<-matrix(c(1,0,0,1),2,2)
+  
+  mg<-rmvnorm(N,mean=c(0,0),sigma=sigma_glob)
+  ml<-rmvnorm(N,mean=c(0,0),sigma=sigma_loc)
+  
+  return(list(mg=mg,
+              ml=ml))
+  
+}
+#----------------------------------------------------------------------------------
 # This function simulates the equation to test asym sensitivity :
 #      P(i,t+1)=beta*P(i,t)+globalnoise(i,t)+alpha*localnoise(i,t)
 # Input :
-#       N : number of global and local noise points you want
+#       mg : global noise matrix (dim = N by 2)
+#       ml : local noise matrix (dim = N by 2)
 #       rho : covariance of global noise matrix
 #       alpha : a number : local noise coeeficient
 #       beta : growth factor of pop
@@ -57,14 +75,9 @@ glob_noise_fn<-function(x,decrease){
 # Output :
 #     pop matrix with dim. = numkeep_last by 2
 
-Simulator_asym_sens<-function(N,rho,alpha,beta,p0=c(0,0),decrease,numkeep_last){
+Simulator_asym_sens<-function(mg,ml,alpha,beta,p0=c(0,0),decrease,numkeep_last){
   
-  sigma_glob<-matrix(c(1,rho,rho,1),2,2)
-  sigma_loc<-matrix(c(1,0,0,1),2,2)
-  
-  mg<-rmvnorm(N,mean=c(0,0),sigma=sigma_glob)
-  ml<-rmvnorm(N,mean=c(0,0),sigma=sigma_loc)
-  
+  N<-nrow(mg)
   g_noise<-apply(mg,MARGIN = 2,FUN = glob_noise_fn, decrease=decrease)
   h_noise<-alpha*ml
   
@@ -84,11 +97,6 @@ Simulator_asym_sens<-function(N,rho,alpha,beta,p0=c(0,0),decrease,numkeep_last){
   
 } 
 #--------------------------------------------------------------------------------------
-
-
-
-
-
 
 
 

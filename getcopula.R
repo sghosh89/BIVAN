d4f8ebf204b899ddf2b7753c6a33daa0676ce21e
sghosh89 <ghoshshyamolina89@gmail.com>
2018-11-05 17:32:31 -0600

    changed...to be continued

--- getcopula.R
+++ getcopula.R
@@ -31,7 +31,7 @@ getcopula<-function(d,rankon=T,ploton=F)
   #plot
   if(ploton==T){
     op<-par(mar=c(5.1, 5.1, 0.2, 2.1))
-    plot(v[,1],v[,2],type="p",col="red",xlab="u",ylab="v",
+    plot(v[,1],v[,2],type="p",xlab="u",ylab="v",
          cex.lab=2.5,cex.axis=1.5,
          xlim=c(0,1),ylim=c(0,1),asp=1)
     rect(0,0,1,1)

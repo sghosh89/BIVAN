ea744c4ec779d04682b7c15c6a6e7cbbdff5584e
sghosh89 <ghoshshyamolina89@gmail.com>
2019-01-11 15:12:59 -0600

    modified

--- 1var_pop_models.R
+++ 1var_pop_models.R
@@ -49,9 +49,14 @@ Hassell<-function(r,a,b,p0,lensim){
 }
 
 #call the function
-Hassell(r=5, a=0.5, b=2.5, p0=0.1, lensim=200) #b~1.5 : contest, b~100 : scramble
+r=5
+a=0.5
+b=1.5
+K_e<-((r^(1/b))-1)/a
+K_e
+Hassell(r=r, a=a, b=b, p0=K_e, lensim=200) #b~1.5 : contest, b~100 : scramble
 
-Hassell(r=4, a=0.5, b=5.1, p0=0.1, lensim=200) # see change : r=5,a=0.5 and vary b 1.5 to 5.5
+Hassell(r=5, a=0.01, b=2.5, p0=0.1, lensim=200) # see change : r=5,a=0.5 and vary b 1.5 to 5.5
 #----------------------------------------------------------------------------------------------------
 # Exploring Maynard Smith model
 Msmith<-function(r,a,b,p0,lensim){
@@ -70,7 +75,7 @@ Msmith<-function(r,a,b,p0,lensim){
 }
 
 #call the function
-Msmith(r=5,a=0.5,b=1.4,p0=0.1,lensim=200) #see changes as r=5,a=0.5 and vary b : 1,1.5,2,4
+Msmith(r=4.5,a=0.5,b=1.4,p0=0.1,lensim=200) #see changes as r=5,a=0.5 and vary b : 1,1.5,2,4
                                           #see changes as a=0.5,b=4 and vary r : 1.2, 1.5, 2
 
 

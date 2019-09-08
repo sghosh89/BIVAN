7312bc1bd518eff6a628430d5312fbd37a16b86a
Dan Reuman <reuman@ku.edu>
2019-09-07 21:06:13 -0500

    adjusting the first pedagog fig

--- FirstPedagogFig.R
+++ FirstPedagogFig.R
@@ -43,7 +43,7 @@ gap<-0.1
 pan_wd<-1
 pan_ht<-pan_wd
 tot_wd<-ymarg_wd+2*gap+2*pan_wd+2*numsp
-tot_ht<-xmarg_ht+4*gap+4*pan_ht+4*numsp
+tot_ht<-xmarg_ht+2*gap+2*pan_ht+2*numsp
 pchval<-20
 cexvalpts<-0.25
 cexvaltxt<-0.75
@@ -53,8 +53,8 @@ pdf(file="./Results/PedagogFig1.pdf",width=tot_wd,height=tot_ht)
 #upper-left panel, normal marginals, left-tail dependence
 par(fig=c((ymarg_wd+numsp)/tot_wd,
           (ymarg_wd+numsp+pan_wd)/tot_wd,
-          (xmarg_ht+3*pan_ht+3*gap+4*numsp)/tot_ht,
-          (xmarg_ht+4*pan_ht+3*gap+4*numsp)/tot_ht),
+          (xmarg_ht+1*pan_ht+1*gap+2*numsp)/tot_ht,
+          (xmarg_ht+2*pan_ht+1*gap+2*numsp)/tot_ht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25)
 rg<-max(abs(dcc5_nm))
 plot(dcc5_nm[,1],dcc5_nm[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(-rg,rg),ylim=c(-rg,rg),col="grey")
@@ -68,8 +68,8 @@ text(rg,-rg,paste0("S=",round(cor(dcc5_nm[,1],dcc5_nm[,2],method="spearman"),2))
 #upper-right panel, normal marginals, right-tail dependence
 par(fig=c((ymarg_wd+2*numsp+pan_wd+gap)/tot_wd,
           (ymarg_wd+2*numsp+2*pan_wd+gap)/tot_wd,
-          (xmarg_ht+3*pan_ht+3*gap+4*numsp)/tot_ht,
-          (xmarg_ht+4*pan_ht+3*gap+4*numsp)/tot_ht),
+          (xmarg_ht+1*pan_ht+1*gap+2*numsp)/tot_ht,
+          (xmarg_ht+2*pan_ht+1*gap+2*numsp)/tot_ht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=T)
 rg<-max(abs(dsc5_nm))
 plot(dsc5_nm[,1],dsc5_nm[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(-rg,rg),ylim=c(-rg,rg),col="grey")
@@ -82,8 +82,8 @@ text(rg,-rg,paste0("S=",round(cor(dsc5_nm[,1],dsc5_nm[,2],method="spearman"),2))
 #2nd row of panels, left, uniform marginals, left-tail dependence
 par(fig=c((ymarg_wd+numsp)/tot_wd,
           (ymarg_wd+numsp+pan_wd)/tot_wd,
-          (xmarg_ht+2*pan_ht+2*gap+3*numsp)/tot_ht,
-          (xmarg_ht+3*pan_ht+2*gap+3*numsp)/tot_ht),
+          (xmarg_ht+0*pan_ht+0*gap+1*numsp)/tot_ht,
+          (xmarg_ht+1*pan_ht+0*gap+1*numsp)/tot_ht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=T)
 #the data
 rg1<-0
@@ -91,12 +91,12 @@ rg2<-1
 plot(dcc5[,1],dcc5[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(rg1,rg2),ylim=c(rg1,rg2),col="grey")
 mtext(expression(z[v]),2,line=1.15)
 #the contours
-x<-seq(from=0,to=1,length.out=101)
-y<-x
-dcarg<-matrix(c(rep(x,times=length(y)),rep(y,each=length(x))),length(x)*length(y),2)
-z<-dCopula(dcarg,cc5)
-z<-matrix(z,101,101)
-contour(x,y,log10(z),nlevels=5,xlim=c(0,1),ylim=c(0,1),col="grey",labels ="",add=T)
+#x<-seq(from=0,to=1,length.out=101)
+#y<-x
+#dcarg<-matrix(c(rep(x,times=length(y)),rep(y,each=length(x))),length(x)*length(y),2)
+#z<-dCopula(dcarg,cc5)
+#z<-matrix(z,101,101)
+#contour(x,y,log10(z),nlevels=5,xlim=c(0,1),ylim=c(0,1),col="grey",labels ="",add=T)
 #contour(x,y,log10(z),nlevels=5,lty=0, add=T)
 #contour(x,y,log10(z),nlevels=5,xlim=c(0,1),ylim=c(0,1))
 text(rg1,rg2,"C",cex=cexvalpl,adj=c(0,1))
@@ -104,61 +104,63 @@ text(rg2,rg1+.125*(rg2-rg1),paste0("P=",round(cor(dcc5[,1],dcc5[,2]),2)),
      adj=c(1,0),cex=cexvaltxt)
 text(rg2,rg1,paste0("S=",round(cor(dcc5[,1],dcc5[,2],method="spearman"),2)),
      adj=c(1,0),cex=cexvaltxt)
+mtext(expression(z[h]),1,line=1.15)
 
 #2nd row of panels, right, uniform marginals, right-tail dependence
 par(fig=c((ymarg_wd+2*numsp+pan_wd+gap)/tot_wd,
           (ymarg_wd+2*numsp+2*pan_wd+gap)/tot_wd,
-          (xmarg_ht+2*pan_ht+2*gap+3*numsp)/tot_ht,
-          (xmarg_ht+3*pan_ht+2*gap+3*numsp)/tot_ht),
+          (xmarg_ht+0*pan_ht+0*gap+1*numsp)/tot_ht,
+          (xmarg_ht+1*pan_ht+0*gap+1*numsp)/tot_ht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=T)
 #the data
 plot(dsc5[,1],dsc5[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(rg1,rg2),ylim=c(rg1,rg2),col="grey")
 #the contours
-x<-seq(from=0,to=1,length.out=101)
-y<-x
-dcarg<-matrix(c(rep(x,times=length(y)),rep(y,each=length(x))),length(x)*length(y),2)
-z<-dCopula(dcarg,sc5)
-z<-matrix(z,101,101)
-contour(x,y,log10(z),nlevels=5,xlim=c(0,1),ylim=c(0,1),col="grey",labels ="",add=T)
+#x<-seq(from=0,to=1,length.out=101)
+#y<-x
+#dcarg<-matrix(c(rep(x,times=length(y)),rep(y,each=length(x))),length(x)*length(y),2)
+#z<-dCopula(dcarg,sc5)
+#z<-matrix(z,101,101)
+#contour(x,y,log10(z),nlevels=5,xlim=c(0,1),ylim=c(0,1),col="grey",labels ="",add=T)
 #contour(x,y,log10(z),nlevels=5,lty=0, add=T)
 text(rg1,rg2,"D",cex=cexvalpl,adj=c(0,1))
 text(rg2,rg1+.125*(rg2-rg1),paste0("P=",round(cor(dsc5[,1],dsc5[,2]),2)),
      adj=c(1,0),cex=cexvaltxt)
 text(rg2,rg1,paste0("S=",round(cor(dsc5[,1],dsc5[,2],method="spearman"),2)),
      adj=c(1,0),cex=cexvaltxt)
+mtext(expression(z[h]),1,line=1.15)
 
 #3th row of panels, left
-par(fig=c((ymarg_wd+numsp)/tot_wd,
-          (ymarg_wd+numsp+pan_wd)/tot_wd,
-          (xmarg_ht+1*pan_ht+1*gap+2*numsp)/tot_ht,
-          (xmarg_ht+2*pan_ht+1*gap+2*numsp)/tot_ht),
-    mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=T)
-dcc5fv<-dcc5
-dcc5fv[,1]<-(-dcc5fv[,1]+1)
-rg1<-0
-rg2<-1
-plot(dcc5fv[,1],dcc5fv[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(rg1,rg2),ylim=c(rg1,rg2),col="grey")
-text(rg2,rg2,"E",cex=cexvalpl,adj=c(1,1))
-text(rg1,rg1+.125*(rg2-rg1),paste0("P=",round(cor(dcc5fv[,1],dcc5fv[,2]),2)),
-     adj=c(0,0),cex=cexvaltxt)
-text(rg1,rg1,paste0("S=",round(cor(dcc5fv[,1],dcc5fv[,2],method="spearman"),2)),
-     adj=c(0,0),cex=cexvaltxt)
-mtext(expression(z[v]),2,line=1.15)
+#par(fig=c((ymarg_wd+numsp)/tot_wd,
+#          (ymarg_wd+numsp+pan_wd)/tot_wd,
+#          (xmarg_ht+1*pan_ht+1*gap+2*numsp)/tot_ht,
+#          (xmarg_ht+2*pan_ht+1*gap+2*numsp)/tot_ht),
+#    mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=T)
+#dcc5fv<-dcc5
+#dcc5fv[,1]<-(-dcc5fv[,1]+1)
+#rg1<-0
+#rg2<-1
+#plot(dcc5fv[,1],dcc5fv[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(rg1,rg2),ylim=c(rg1,rg2),col="grey")
+#text(rg2,rg2,"E",cex=cexvalpl,adj=c(1,1))
+#text(rg1,rg1+.125*(rg2-rg1),paste0("P=",round(cor(dcc5fv[,1],dcc5fv[,2]),2)),
+#     adj=c(0,0),cex=cexvaltxt)
+#text(rg1,rg1,paste0("S=",round(cor(dcc5fv[,1],dcc5fv[,2],method="spearman"),2)),
+#     adj=c(0,0),cex=cexvaltxt)
+#mtext(expression(z[v]),2,line=1.15)
 
 #3th row of panels, right
-par(fig=c((ymarg_wd+2*numsp+pan_wd+gap)/tot_wd,
-          (ymarg_wd+2*numsp+2*pan_wd+gap)/tot_wd,
-          (xmarg_ht+1*pan_ht+1*gap+2*numsp)/tot_ht,
-          (xmarg_ht+2*pan_ht+1*gap+2*numsp)/tot_ht),
-    mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=T)
-dsc5fv<-dsc5
-dsc5fv[,1]<-(-dsc5fv[,1]+1)
-plot(dsc5fv[,1],dsc5fv[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(rg1,rg2),ylim=c(rg1,rg2),col="grey")
-text(rg2,rg2,"F",cex=cexvalpl,adj=c(1,1))
-text(rg1,rg1+.125*(rg2-rg1),paste0("P=",round(cor(dsc5fv[,1],dsc5fv[,2]),2)),
-     adj=c(0,0),cex=cexvaltxt)
-text(rg1,rg1,paste0("S=",round(cor(dsc5fv[,1],dsc5fv[,2],method="spearman"),2)),
-     adj=c(0,0),cex=cexvaltxt)
+#par(fig=c((ymarg_wd+2*numsp+pan_wd+gap)/tot_wd,
+#          (ymarg_wd+2*numsp+2*pan_wd+gap)/tot_wd,
+#          (xmarg_ht+1*pan_ht+1*gap+2*numsp)/tot_ht,
+#          (xmarg_ht+2*pan_ht+1*gap+2*numsp)/tot_ht),
+#    mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=T)
+#dsc5fv<-dsc5
+#dsc5fv[,1]<-(-dsc5fv[,1]+1)
+#plot(dsc5fv[,1],dsc5fv[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(rg1,rg2),ylim=c(rg1,rg2),col="grey")
+#text(rg2,rg2,"F",cex=cexvalpl,adj=c(1,1))
+#text(rg1,rg1+.125*(rg2-rg1),paste0("P=",round(cor(dsc5fv[,1],dsc5fv[,2]),2)),
+#     adj=c(0,0),cex=cexvaltxt)
+#text(rg1,rg1,paste0("S=",round(cor(dsc5fv[,1],dsc5fv[,2],method="spearman"),2)),
+#     adj=c(0,0),cex=cexvaltxt)
 
 #4th row of panels, left - normal copula data and contours
 par(fig=c((ymarg_wd+numsp)/tot_wd,
@@ -167,41 +169,41 @@ par(fig=c((ymarg_wd+numsp)/tot_wd,
           (xmarg_ht+1*pan_ht+0*gap+1*numsp)/tot_ht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=T)
 #data
-plot(dnc[,1],dnc[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(rg1,rg2),ylim=c(rg1,rg2),col="grey")
-#text(rg2,rg1+.125*(rg2-rg1),paste0("P=",round(cor(dnc[,1],dnc[,2]),2)),
-#     adj=c(1,0),cex=cexvaltxt)
-#text(rg2,rg1,paste0("S=",round(cor(dnc[,1],dnc[,2],method="spearman"),2)),
-#     adj=c(1,0),cex=cexvaltxt)
-#contours
-x<-seq(from=0,to=1,length.out=101)
-y<-x
-dcarg<-matrix(c(rep(x,times=length(y)),rep(y,each=length(x))),length(x)*length(y),2)
-z<-dCopula(dcarg,nc,log=FALSE)
-z<-matrix(z,101,101)
-contour(x,y,log10(z),nlevels=5,xlim=c(0,1),ylim=c(0,1),xaxt="n",col="grey",labels ="",add=T)
-#contour(x,y,log10(z),nlevels=5,lty=0, add=T)
-axis(side=1,labels=FALSE)
-text(rg1,rg2,"G",cex=cexvalpl,adj=c(0,1))
-mtext(expression(z[h]),1,line=1.15)
-mtext(expression(z[v]),2,line=1.15)
+#plot(dnc[,1],dnc[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(rg1,rg2),ylim=c(rg1,rg2),col="grey")
+##text(rg2,rg1+.125*(rg2-rg1),paste0("P=",round(cor(dnc[,1],dnc[,2]),2)),
+##     adj=c(1,0),cex=cexvaltxt)
+##text(rg2,rg1,paste0("S=",round(cor(dnc[,1],dnc[,2],method="spearman"),2)),
+##     adj=c(1,0),cex=cexvaltxt)
+##contours
+#x<-seq(from=0,to=1,length.out=101)
+#y<-x
+#dcarg<-matrix(c(rep(x,times=length(y)),rep(y,each=length(x))),length(x)*length(y),2)
+#z<-dCopula(dcarg,nc,log=FALSE)
+#z<-matrix(z,101,101)
+#contour(x,y,log10(z),nlevels=5,xlim=c(0,1),ylim=c(0,1),xaxt="n",col="grey",labels ="",add=T)
+##contour(x,y,log10(z),nlevels=5,lty=0, add=T)
+#axis(side=1,labels=FALSE)
+#text(rg1,rg2,"G",cex=cexvalpl,adj=c(0,1))
+#mtext(expression(z[h]),1,line=1.15)
+#mtext(expression(z[v]),2,line=1.15)
 
-#4th row of panels, right - figure to get cor_lb,ub across
-par(fig=c((ymarg_wd+2*numsp+pan_wd+gap)/tot_wd,
-          (ymarg_wd+2*numsp+2*pan_wd+gap)/tot_wd,
-          (xmarg_ht+0*pan_ht+0*gap+1*numsp)/tot_ht,
-          (xmarg_ht+1*pan_ht+0*gap+1*numsp)/tot_ht),
-    mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=T)
-plot(dcc5[,1],dcc5[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(rg1,rg2),ylim=c(rg1,rg2),col="grey")
-lines(c(0,.4),c(.4,0),type='l')
-lines(c(0,.65),c(.65,0),type='l')
-lines(c(0,1.6),c(1.6,0),type='l')
-lines(c(0,1.35),c(1.35,0),type='l')
-mtext(expression(z[h]),1,line=1.15)
-text(rg1,rg2,"H",cex=cexvalpl,adj=c(0,1))
+##4th row of panels, right - figure to get cor_lb,ub across
+#par(fig=c((ymarg_wd+2*numsp+pan_wd+gap)/tot_wd,
+#          (ymarg_wd+2*numsp+2*pan_wd+gap)/tot_wd,
+#          (xmarg_ht+0*pan_ht+0*gap+1*numsp)/tot_ht,
+#          (xmarg_ht+1*pan_ht+0*gap+1*numsp)/tot_ht),
+#    mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=T)
+#plot(dcc5[,1],dcc5[,2],type='p',pch=pchval,cex=cexvalpts,xlim=c(rg1,rg2),ylim=c(rg1,rg2),col="grey")
+#lines(c(0,.4),c(.4,0),type='l')
+#lines(c(0,.65),c(.65,0),type='l')
+#lines(c(0,1.6),c(1.6,0),type='l')
+#lines(c(0,1.35),c(1.35,0),type='l')
+#mtext(expression(z[h]),1,line=1.15)
+#text(rg1,rg2,"H",cex=cexvalpl,adj=c(0,1))
 
 dev.off()
 
-#***a pedagog fig for the sup mat about monotnic transformations
+#***a pedagog fig for the sup mat about monotonic transformations
 
 #plot dimensions, units inches
 xmarg_ht<-.25

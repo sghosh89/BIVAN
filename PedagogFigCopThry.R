5eb906a3e4641b02049dfc3ac07ed742bbe08d38
Dan Reuman <reuman@ku.edu>
2019-09-10 09:20:27 -0500

    still working on Background section

--- PedagogFigCopThry.R
+++ PedagogFigCopThry.R
@@ -3,8 +3,6 @@
 #can be combined with different marginals to give different distributions
 #***
 
-rm(list=ls())
-
 library(copula)
 
 #***make the two copulas
@@ -38,20 +36,21 @@ biggap<-.2
 smallgap<-.1
 xmarht<-.5
 ymarwd<-xmarht
-panwd<-(totwd-3*ymarwd-2*biggap-4*smallgap)/(4)
+textspace<-0.2
+panwd<-(totwd-3*ymarwd-2*biggap-4*smallgap-textspace)/(4)
 margpansm<-panwd/3
 panht<-panwd
 margpanbg<-panht
-totht<-2*xmarht+2*panht+2*margpansm+biggap+3*smallgap
-if (!isTRUE(all.equal(totwd,3*ymarwd+3*panwd+3*margpansm+2*biggap+4*smallgap)))
+totht<-2*xmarht+2*panht+2*margpansm+biggap+3*smallgap+textspace
+if (!isTRUE(all.equal(totwd,3*ymarwd+3*panwd+3*margpansm+2*biggap+4*smallgap+textspace)))
 {
   stop("Error in figure dimensions")
 }
 pdf(file="./Results/PedagogFigCopThry.pdf",width=totwd,height=totht)
 
 #**panel for contours for the normal cop
-par(fig=c((ymarwd)/totwd,
-          (ymarwd+panwd)/totwd,
+par(fig=c((textspace+ymarwd)/totwd,
+          (textspace+ymarwd+panwd)/totwd,
           (2*xmarht+panht+margpansm+smallgap+biggap)/totht,
           (2*xmarht+2*panht+margpansm+smallgap+biggap)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25)
@@ -69,6 +68,7 @@ contour(x,y,log10(z),levels=levs,lty=0,add=TRUE)
 mtext("u",1,1.2)
 mtext("v",2,1.2)
 mtext("A",3,.8,at=c(-.35))
+mtext("NORMAL COPULA",2,2.4)
 
 #***add samples
 set.seed(101)
@@ -76,8 +76,8 @@ samps_ncop<-rCopula(numsamps,ncop)
 points(samps_ncop[,1],samps_ncop[,2],type="p",pch=20,col="grey",cex=.5)
 
 #**normal copula plot, top marginal
-par(fig=c((ymarwd)/totwd,
-          (ymarwd+margpanbg)/totwd,
+par(fig=c((textspace+ymarwd)/totwd,
+          (textspace+ymarwd+margpanbg)/totwd,
           (2*xmarht+2*panht+margpansm+2*smallgap+biggap)/totht,
           (2*xmarht+2*panht+2*margpansm+2*smallgap+biggap)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -88,10 +88,11 @@ y<-rep(1,length(x))
 plot(x,y,type='l',yaxt="n",xaxt="n")
 rug(samps_ncop[,1],ticksize=0.06)
 axis(side=1,labels=FALSE)
+mtext("UNIFORM MARGINALS",3,0.2)
 
 #**normal copula plot, right marginal
-par(fig=c((ymarwd+panwd+smallgap)/totwd,
-          (ymarwd+panwd+smallgap+margpansm)/totwd,
+par(fig=c((textspace+ymarwd+panwd+smallgap)/totwd,
+          (textspace+ymarwd+panwd+smallgap+margpansm)/totwd,
           (2*xmarht+panht+margpansm+smallgap+biggap)/totht,
           (2*xmarht+2*panht+margpansm+smallgap+biggap)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -104,8 +105,8 @@ rug(samps_ncop[,2],ticksize=0.06,side=2)
 axis(side=2,labels=FALSE)
 
 #**first normal copula example, main panel
-par(fig=c((2*ymarwd+panwd+biggap+margpansm)/totwd,
-          (2*ymarwd+2*panwd+biggap+margpansm)/totwd,
+par(fig=c((textspace+2*ymarwd+panwd+biggap+margpansm)/totwd,
+          (textspace+2*ymarwd+2*panwd+biggap+margpansm)/totwd,
           (2*xmarht+panht+margpansm+smallgap+biggap)/totht,
           (2*xmarht+2*panht+margpansm+smallgap+biggap)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -129,8 +130,8 @@ samps<-qnmarg(samps_ncop)
 points(samps[,1],samps[,2],type='p',pch=20,cex=0.5,col="grey")
   
 #**first normal copula example, top marginal
-par(fig=c((2*ymarwd+panwd+biggap+margpansm)/totwd,
-          (2*ymarwd+2*panwd+biggap+margpansm)/totwd,
+par(fig=c((textspace+2*ymarwd+panwd+biggap+margpansm)/totwd,
+          (textspace+2*ymarwd+2*panwd+biggap+margpansm)/totwd,
           (2*xmarht+2*panht+margpansm+2*smallgap+biggap)/totht,
           (2*xmarht+2*panht+2*margpansm+2*smallgap+biggap)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -141,10 +142,11 @@ y<-dnmarg(x)
 plot(x,y,type='l',yaxt="n",xaxt="n")
 rug(samps[,1],ticksize=0.06)
 axis(side=1,labels=FALSE)
+mtext("NORMAL MARGINALS",3,0.2)
 
 #**first normal copula example, right marginal
-par(fig=c((2*ymarwd+2*panwd+biggap+smallgap+margpansm)/totwd,
-          (2*ymarwd+2*panwd+biggap+smallgap+margpansm+margpansm)/totwd,
+par(fig=c((textspace+2*ymarwd+2*panwd+biggap+smallgap+margpansm)/totwd,
+          (textspace+2*ymarwd+2*panwd+biggap+smallgap+margpansm+margpansm)/totwd,
           (2*xmarht+panht+margpansm+smallgap+biggap)/totht,
           (2*xmarht+2*panht+margpansm+smallgap+biggap)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -157,8 +159,8 @@ rug(samps[,2],ticksize=0.06,side=2)
 axis(side=2,labels=FALSE)
 
 #**second normal copula example, main panel
-par(fig=c((3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
-          (3*ymarwd+3*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
+par(fig=c((textspace+3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
+          (textspace+3*ymarwd+3*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
           (2*xmarht+panht+margpansm+smallgap+biggap)/totht,
           (2*xmarht+2*panht+margpansm+smallgap+biggap)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -182,8 +184,8 @@ samps<-qgmarg(samps_ncop)
 points(samps[,1],samps[,2],type='p',pch=20,cex=0.5,col="grey")
 
 #**second normal copula example, top marginal
-par(fig=c((3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
-          (3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpanbg+margpansm)/totwd,
+par(fig=c((textspace+3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
+          (textspace+3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpanbg+margpansm)/totwd,
           (2*xmarht+2*panht+margpansm+2*smallgap+biggap)/totht,
           (2*xmarht+2*panht+2*margpansm+2*smallgap+biggap)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -194,10 +196,11 @@ y<-dgmarg(x)
 plot(x,y,type='l',yaxt="n",xaxt="n")
 rug(samps[,1],ticksize=0.06)
 axis(side=1,labels=FALSE)
+mtext("GAMMA MARGINALS",3,0.2)
 
 #**second normal copula example, right marginal
-par(fig=c((3*ymarwd+3*panwd+2*biggap+2*smallgap+margpansm+margpansm)/totwd,
-          (3*ymarwd+3*panwd+2*biggap+2*smallgap+2*margpansm+margpansm)/totwd,
+par(fig=c((textspace+3*ymarwd+3*panwd+2*biggap+2*smallgap+margpansm+margpansm)/totwd,
+          (textspace+3*ymarwd+3*panwd+2*biggap+2*smallgap+2*margpansm+margpansm)/totwd,
           (2*xmarht+panht+margpansm+smallgap+biggap)/totht,
           (2*xmarht+2*panht+margpansm+smallgap+biggap)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -210,8 +213,8 @@ rug(samps[,2],ticksize=0.06,side=2)
 axis(side=2,labels=FALSE)
 
 #**panel for contours for the clayton cop
-par(fig=c((ymarwd)/totwd,
-          (ymarwd+panwd)/totwd,
+par(fig=c((textspace+ymarwd)/totwd,
+          (textspace+ymarwd+panwd)/totwd,
           (xmarht)/totht,
           (xmarht+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -229,6 +232,7 @@ contour(x,y,log10(z),levels=levs,lty=0,add=TRUE)
 mtext("u",1,1.2)
 mtext("v",2,1.2)
 mtext("B",3,.8,at=c(-.35))
+mtext("CLAYTON COPULA",2,2.4)
 
 #***add samples
 set.seed(107)
@@ -236,8 +240,8 @@ samps_ccop<-rCopula(numsamps,ccop)
 points(samps_ccop[,1],samps_ccop[,2],type="p",pch=20,col="grey",cex=.5)
 
 #**clayton copula plot, top marginal
-par(fig=c((ymarwd)/totwd,
-          (ymarwd+margpanbg)/totwd,
+par(fig=c((textspace+ymarwd)/totwd,
+          (textspace+ymarwd+margpanbg)/totwd,
           (xmarht+panht+smallgap)/totht,
           (xmarht+panht+smallgap+margpansm)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -250,8 +254,8 @@ rug(samps_ccop[,1],ticksize=0.06)
 axis(side=1,labels=FALSE)
 
 #**clayton copula plot, right marginal
-par(fig=c((ymarwd+panwd+smallgap)/totwd,
-          (ymarwd+panwd+smallgap+margpansm)/totwd,
+par(fig=c((textspace+ymarwd+panwd+smallgap)/totwd,
+          (textspace+ymarwd+panwd+smallgap+margpansm)/totwd,
           (xmarht)/totht,
           (xmarht+margpanbg)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -264,8 +268,8 @@ rug(samps_ccop[,2],ticksize=0.06,side=2)
 axis(side=2,labels=FALSE)
 
 #**first clayton copula example, main panel
-par(fig=c((2*ymarwd+panwd+biggap+margpansm)/totwd,
-          (2*ymarwd+2*panwd+biggap+margpansm)/totwd,
+par(fig=c((textspace+2*ymarwd+panwd+biggap+margpansm)/totwd,
+          (textspace+2*ymarwd+2*panwd+biggap+margpansm)/totwd,
           (1*xmarht)/totht,
           (1*xmarht+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -288,8 +292,8 @@ samps<-qnmarg(samps_ccop)
 points(samps[,1],samps[,2],type='p',pch=20,cex=0.5,col="grey")
 
 #**first clayton copula example, top marginal
-par(fig=c((2*ymarwd+panwd+biggap+margpansm)/totwd,
-          (2*ymarwd+2*panwd+biggap+margpansm)/totwd,
+par(fig=c((textspace+2*ymarwd+panwd+biggap+margpansm)/totwd,
+          (textspace+2*ymarwd+2*panwd+biggap+margpansm)/totwd,
           (xmarht+panht+smallgap)/totht,
           (xmarht+panht+smallgap+margpansm)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -302,8 +306,8 @@ rug(samps[,1],ticksize=0.06)
 axis(side=1,labels=FALSE)
 
 #**first clayton copula example, right marginal
-par(fig=c((2*ymarwd+2*panwd+biggap+smallgap+margpansm)/totwd,
-          (2*ymarwd+2*panwd+biggap+smallgap+margpansm+margpansm)/totwd,
+par(fig=c((textspace+2*ymarwd+2*panwd+biggap+smallgap+margpansm)/totwd,
+          (textspace+2*ymarwd+2*panwd+biggap+smallgap+margpansm+margpansm)/totwd,
           (xmarht)/totht,
           (xmarht+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -316,8 +320,8 @@ rug(samps[,2],ticksize=0.06,side=2)
 axis(side=2,labels=FALSE)
 
 #**second clayton copula example, main panel
-par(fig=c((3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
-          (3*ymarwd+3*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
+par(fig=c((textspace+3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
+          (textspace+3*ymarwd+3*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
           (xmarht)/totht,
           (xmarht+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -341,8 +345,8 @@ samps<-qgmarg(samps_ccop)
 points(samps[,1],samps[,2],type='p',pch=20,cex=0.5,col="grey")
 
 #**second clayton copula example, top marginal
-par(fig=c((3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
-          (3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpanbg+margpansm)/totwd,
+par(fig=c((textspace+3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpansm)/totwd,
+          (textspace+3*ymarwd+2*panwd+2*biggap+smallgap+margpansm+margpanbg+margpansm)/totwd,
           (xmarht+panht+smallgap)/totht,
           (xmarht+panht+margpansm+smallgap)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
@@ -355,8 +359,8 @@ rug(samps[,1],ticksize=0.06)
 axis(side=1,labels=FALSE)
 
 #**second clayton copula example, right marginal
-par(fig=c((3*ymarwd+3*panwd+2*biggap+2*smallgap+margpansm+margpansm)/totwd,
-          (3*ymarwd+3*panwd+2*biggap+2*smallgap+2*margpansm+margpansm)/totwd,
+par(fig=c((textspace+3*ymarwd+3*panwd+2*biggap+2*smallgap+margpansm+margpansm)/totwd,
+          (textspace+3*ymarwd+3*panwd+2*biggap+2*smallgap+2*margpansm+margpansm)/totwd,
           (xmarht)/totht,
           (xmarht+margpanbg)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)

0264f0e9d350e2ebd224eeaef57dde4f6fe86428
sghosh89 <ghoshshyamolina89@gmail.com>
2019-09-29 01:32:52 -0500

    table fixed as suggested by DCR

--- normal_cop_AICw.R
+++ normal_cop_AICw.R
@@ -1,9 +1,11 @@
-# This function gives you a matrix where its [i,j] element is the AICw for a fitted normal copula for the location pair [i,j]
+# This function gives you a list of two matrices where its [i,j] element are the AICw and AIC 
+# for a fitted normal copula for the location pair [i,j]
 # Input : 
 #       r is the RDS file which came as an output from FittingCopula_selective_loc.R
 
 normal_cop_AICw_matrix<-function(r){
   normal_cop_AICw<-r$gfc_numBS
+  normal_cop_AIC<-normal_cop_AICw
   #matrix(NA, nrow=dim(r$gfc_p_CvM)[1], ncol=dim(r$gfc_p_CvM)[2])
   #colnames(normal_cop_AICw)<-colnames(r$gfc_p_CvM)
   #rownames(normal_cop_AICw)<-rownames(r$gfc_p_CvM)
@@ -15,11 +17,14 @@ normal_cop_AICw_matrix<-function(r){
         norm_ind<-which(x==1)
         norm_AICw<-r$info_ord_AICw[i,j,norm_ind]
         normal_cop_AICw[i,j]<-norm_AICw
+        norm_AIC<-r$info_ord_AIC[i,j,norm_ind]
+        normal_cop_AIC[i,j]<-norm_AIC
       }
     }
   } 
   
-  return(normal_cop_AICw)
+  return(list(normal_cop_AICw=normal_cop_AICw,
+              normal_cop_AIC=normal_cop_AIC))
   
 }
 
